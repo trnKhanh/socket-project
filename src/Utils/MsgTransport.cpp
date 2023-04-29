52906426049c0a64f6d89e203ec27d1b20fdd8c5
trnKhanh <tnk29082003@gmail.com>
2023-04-29 15:22:59 +0700

    fix bugs and add new utils

--- src/Utils/MsgTransport.cpp
+++ src/Utils/MsgTransport.cpp
@@ -7,7 +7,7 @@ int sendAll(int sockfd, const void *msg, size_t len, int flag)
 
     while (totalByteSent < len)
     {
-        int byteSent = send(sockfd, msg + totalByteSent, len - totalByteSent, flag);
+        int byteSent = send(sockfd, (char*)msg + totalByteSent, len - totalByteSent, flag);
 
         if (byteSent <= 0)
             return -1;
@@ -22,7 +22,38 @@ int recvAll(int sockfd, void *msg, size_t len, int flag)
     int totalByteRecv = 0;
     while (totalByteRecv < len)
     {
-        int byteRecv = recv(sockfd, msg + totalByteRecv, len - totalByteRecv, flag);
+        int byteRecv = recv(sockfd, (char*)msg + totalByteRecv, len - totalByteRecv, flag);
+
+        if (byteRecv <= 0)
+            return -1;
+
+        totalByteRecv += byteRecv;
+    }
+
+    return 0;
+}
+
+int sendtoAll(int sockfd, const void *msg, size_t len, int flag, const sockaddr* addr, socklen_t addrlen)
+{
+    int totalByteSent = 0;
+
+    while (totalByteSent < len)
+    {
+        int byteSent = sendto(sockfd, (char*)msg + totalByteSent, len - totalByteSent, flag, addr, addrlen);
+
+        if (byteSent <= 0)
+            return -1;
+
+        totalByteSent += byteSent;
+    }
+    return 0;
+}
+int recvfromAll(int sockfd, void *msg, size_t len, int flag, sockaddr* addr, socklen_t *addrlen)
+{
+    int totalByteRecv = 0;
+    while (totalByteRecv < len)
+    {
+        int byteRecv = recvfrom(sockfd, (char*)msg + totalByteRecv, len - totalByteRecv, flag, addr, addrlen);
 
         if (byteRecv <= 0)
             return -1;
--- src/Utils/MsgTransport.h
+++ src/Utils/MsgTransport.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <iostream>
+#include <arpa/inet.h>
 
 int sendAll(int sockfd, const void *msg, size_t len, int flag);
 int recvAll(int sockfd, void *msg, size_t len, int flag);
+
+int sendtoAll(int sockfd, const void *msg, size_t len, int flag, const sockaddr* addr, socklen_t addrlen);
+int recvfromAll(int sockfd, void *msg, size_t len, int flag, sockaddr* addr, socklen_t *addrlen);
