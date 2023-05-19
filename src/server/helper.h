cab6eb4426b4007b06c7dcf5c457d9dcc597f496
trnKhanh <tnk29082003@gmail.com>
2023-05-19 17:14:25 +0700

    change helper

--- src/server/helper.h
+++ src/server/helper.h
@@ -4,11 +4,10 @@
     #include "function_unix/AppCMD.h"
     #include "function_unix/Screenshot.h"
     #include "function_unix/Keylog.h"
-#elif __WIN32__
+    #include "function_unix/ListDirTree.h"
+#elif _WIN32
     #include "function_window/ListProcesses.h"
     #include "function_window/AppCMD.h"
     #include "function_window/Screenshot.h"
     #include "function_window/Keylog.h"
 #endif
-
-#include "function_cross/ListDirTree.h"
